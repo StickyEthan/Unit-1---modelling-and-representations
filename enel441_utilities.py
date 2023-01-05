544a8eb56f3aa194a844c942b75a72ac4b55685a
arneUofC <arne.dankers2@ucalgary.ca>
2023-01-04 23:40:32 -0700

    end of day

--- enel441_utilities.py
+++ enel441_utilities.py
@@ -14,6 +14,41 @@ def partial_fraction_expansion_3nd_order_sys(num, den):
     coefficients_vec = np.linalg.inv(regression_matrix) @ num_vec  #note @ means matrix multiply in python
     return poles, coefficients_vec
 
+def eval_poly(poly, k):
+    n = poly.shape[0] - 1
+
+    p_at_k = 0
+    for ii in range(n+1):
+        p_at_k += poly[ii]*k**(n-ii)
+    
+    return p_at_k
+
+def enel441_partial_fraction_expansion(num, den):
+
+    poles = np.roots(den)
+    np = poles.shape[0]
+    coeff = np.zeros(np)
+
+    for ii in range(np):
+        num = eval_poly(poles[ii])
+
+        den = 1
+        for jj in range(np-1):
+            if ii != jj:
+                den *= (poles[ii] - poles[jj])
+        
+        coeff[ii] = num/den
+    return poles, coeff
+            
+            
+
+
+    regression_matrix = np.array([[1,1,1], [poles[1]+poles[2], poles[0]+poles[2], poles[0]+poles[1]], [poles[1]*poles[2], poles[0]*poles[2], poles[0]*poles[1]]])
+    num_vec = np.zeros(3)
+    num_vec[(3-num.shape[0]):3] = num
+
+    coefficients_vec = np.linalg.inv(regression_matrix) @ num_vec  #note @ means matrix multiply in python
+    return poles, coefficients_vec
 
 def enel441_step_response(num, den, t):
     poles, coefficients = partial_fraction_expansion_3nd_order_sys(num, den)
@@ -24,6 +59,11 @@ def enel441_step_response(num, den, t):
     ax.plot(t,out_step.real)
     ax.set_title(r'Step response')
     ax.set_xlabel('Time (s)')
+
+    plt.xlim(t[0], t[-1])
+    y_bottom, y_top = plt.ylim()
+    plt.ylim(0, y_top)
+
     return fig, ax, out_step
 
 
@@ -102,4 +142,51 @@ def enel441_settling_time(t, out_step, ax):
     
     return t[index_settling_time]
 
+
+def enel441_s_plane_plot(num_sys, den_sys, fig=[], ax=[]):
+    if not fig: 
+        fig, ax = plt.subplots(1,1)
+    
+    poles_sys = np.roots(den_sys)
+    for pp in poles_sys:
+        ax.plot(np.real(pp), np.imag(pp), 'bx' )
+
+    zeros_sys = np.roots(num_sys)
+    for zz in zeros_sys:
+        ax.plot(np.real(zz), np.imag(zz), 'ro')
+
+    plt.xlim(np.min(np.real(poles_sys))-0.5, np.max(np.real(poles_sys))+0.5)
+    x_left, x_right = plt.xlim()
+
+    if x_right < 0.5:
+        x_right = 0.5
+
+    if x_left > -0.5:
+        x_left = -0.5
+
+    plt.xlim(x_left, x_right)
+    ax.grid(True)
+    ax.spines['left'].set_position('zero')
+    # turn off the right spine/ticks
+    ax.spines['right'].set_color('none')
+    ax.yaxis.tick_left()
+    # set the y-spine
+    ax.spines['bottom'].set_position('zero')
+
+    # turn off the top spine/ticks
+    ax.spines['top'].set_color('none')
+    ax.xaxis.tick_bottom()
+    ax.set_title('S-Plane Plot')
+    ax.set_xlabel('Real[s]')
+    ax.set_ylabel('Imag[s]')
+
+    ax.xaxis.set_label_coords(0.5,-0.01)
+    ax.yaxis.set_label_coords(-0.01,0.5)
+    return fig, ax
+
+def roots_to_polynomial(roots_poly):
+    poly = np.array([1])
+    for rr in roots_poly:
+        poly = np.convolve(poly, np.array([1, -rr]))
+    return np.real(poly)
 
